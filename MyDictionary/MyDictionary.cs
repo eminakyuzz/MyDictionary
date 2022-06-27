6a7182987c31aa2156e6bd9bbb1e8c1a0c47f3fc
emin <akyuzz.eminn@gmail.com>
2022-06-27 18:03:26 +0300

    Add project files.

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyDictionary
+{
+    class MyDictionary<K,V>
+    {
+        K[] keys;
+        V[] values;
+        public MyDictionary()
+        {
+            keys = new K[0];
+            values = new V[0];
+        }
+        public void Add(K key,V value)
+        {
+            K[] tempArray = keys;
+            V[] tempArray2 = values;
+
+            keys = new K[keys.Length + 1];
+            values = new V[values.Length + 1];
+
+            for (int i = 0; i < tempArray.Length; i++)
+            {
+                keys[i] = tempArray[i];
+            }
+            keys[keys.Length - 1] = key;
+
+            for (int i = 0; i < tempArray2.Length; i++)
+            {
+                values[i] = tempArray2[i];
+            }
+            values[values.Length - 1] = value;
+        }
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> members = new MyDictionary<int, string>();
+
+            members.Add(10, "emin");
+            members.Add(20, "muhammed");
+
+            
+        }
+    }
+}
